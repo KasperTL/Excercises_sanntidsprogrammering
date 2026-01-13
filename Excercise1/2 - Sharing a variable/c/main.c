4861be264291957b730bd88fa7da84bc6182f825
KasperTL <kasper.langland@gmail.com>
2026-01-13 13:23:17 +0100

    changes i folders Excercise1

---
+++ Excercise1/2 - Sharing a variable/c/main.c
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <stdio.h>
+
+
+
+int i = 0;
+
+void* incrementingThreadFunction(){
+    // TODO: increment i 1_000_000 times
+    for (int j = 0; j < 1000000; j++) {
+        i++;
+    };
+    return NULL;
+}
+
+void* decrementingThreadFunction(){
+    // TODO: decrement i 1_000_000 times
+    for (int j = 0; j < 1000000; j++) {
+        i--;
+    };
+    return NULL;
+};
+
+void* decrementingThreadFunction(){
+    // TODO: decrement i 1_000_000 times
+    for (int i = 0; i < 1000000; i++); {
+        i--;
+    };
+    return NULL;
+}
+
+
+int main() {
+    pthread_t thread_1, thread_2;
+
+    pthread_create(&thread_1, NULL, incrementingThreadFunction(), NULL);
+    pthread_create(&thread_2, NULL, decrementingThreadFunction(), NULL);
+
+    pthread_join(thread_1, NULL);
+    pthread_join(thread_2, NULL);
+
+    printf("The magic number is: %d\n", i);
+
+};
